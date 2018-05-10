a18652493c663936709bbe3f409a50c31408d23d
Luis Díaz Más <piponazo@gmail.com>
2018-05-10 14:48:50 +0200

    Add basic cmake code + conan to handle GTest dependency

---
+++ conanfile.py
@@ -0,0 +1,15 @@
+from conans import ConanFile
+
+class CtCIConan(ConanFile):
+    settings = 'os', 'compiler', 'build_type', 'arch'
+    generators = 'cmake'
+
+    def configure(self):
+        self.options['gtest'].shared = True
+
+    def requirements(self):
+        self.requires('gtest/1.8.0@bincrafters/stable')
+
+    def imports(self):
+        self.copy('*.dll', dst='bin', src='bin')
+        self.copy('*.dylib', dst='bin', src='lib')
