65eb435f591098372968082f6e2ce83c4168e2ae
Luis Díaz Más <piponazo@gmail.com>
2018-10-18 10:00:40 +0200

    Review 1.2
    - Add 1.2 to the test names.
    - Use std::any_of to simplify the final for-range loop.
    - Notes about complexity
    - clang-format

--- ch1_arrays_and_strings/luis/1.2_check_permutation.cpp
+++ ch1_arrays_and_strings/luis/1.2_check_permutation.cpp
@@ -1,61 +1,74 @@
-// Check Permutation: Given two strings, write a method to decide if one is a permutation of the
-// other.
+// Check Permutation: Given two strings, write a method to decide if one is a
+// permutation of the other.
+// Notes: 1) Ask yourself if chars are ASCII (128), Extended ascii (256) or Unicode ( >100.000)
+//        2) Case sensitive?
 
 #include "catch.hpp"
 
+#include <algorithm>
+#include <cstdint>
 #include <string>
 #include <unordered_map>
-#include <algorithm>
 
 using namespace std;
 
-// This solution does not use any additional space, but time complexity is O(N log N)
-bool isPermutationSort(string& ref, string& comp) {
+// Time complexity:  O(NlogN)
+// Space complexity: O(1)
+bool isPermutationSort(string &ref, string &comp)
+{
     sort(begin(ref), end(ref));
     sort(begin(comp), end(comp));
 
     // Some day in a near future ...
-    //sort(std::execution::par_unseq, begin(ref), end(ref));
-    //sort(std::execution::par_unseq, begin(comp), end(comp));
+    // sort(std::execution::par_unseq, begin(ref), end(ref));
+    // sort(std::execution::par_unseq, begin(comp), end(comp));
     return ref == comp;
 }
 
-// This solution uses some space but its runtime is O(N)
-bool isPermutation(const string& ref, const string& comp) {
+// Time complexity:  O(N)
+// Space complexity: O(N)
+bool isPermutation(const string &ref, const string &comp)
+{
     if (ref.size() != comp.size()) {
         return false;
     }
 
-    unordered_map<char, int> charsInRef;
-    for (auto c: ref) {
-        charsInRef[c]++;
-    }
-
-    for (auto c: comp) {
-        charsInRef[c]--;
-        if (charsInRef[c]<0) {
-            return false;
-        }
-    }
+    // Note that the values could be ints of 8, 16, 32 or 64 bytes depending on the size of the strings to process.
+    unordered_map<char, std::int8_t> charsInRef;
+    for_each(begin(ref), end(ref), [&charsInRef](char c) { charsInRef[c]++; });
+    return !any_of(begin(comp), end(comp), [&charsInRef](char c) { return --charsInRef[c] < 0; });
 }
 
-TEST_CASE("It is permutation (with unique chars)") {
-    string str1 ("asdfghjklñ");
-    string str2 ("ñlkjhgfdsa");
+TEST_CASE("1.2 - Returns true with empty strings")
+{
+    string str1("");
+    string str2("");
     REQUIRE(isPermutation(str1, str2));
     REQUIRE(isPermutationSort(str1, str2));
 }
 
-TEST_CASE("It is permutation (with repeated chars)") {
-    string str1 ("asdfghjklñd");
-    string str2 ("ñlkjhgfdsad");
+TEST_CASE("1.2 - Returns true with permutations of strings with unique chars")
+{
+    string str1("asdfghjklñ");
+    string str2("ñlkjhgfdsa");
     REQUIRE(isPermutation(str1, str2));
     REQUIRE(isPermutationSort(str1, str2));
 }
 
-TEST_CASE("It isn't permutation") {
-    string str1 ("asdfghjklñ");
-    string str2 ("ñlkjhgfdso");
+TEST_CASE(
+    "1.2 - Returns true with permutations of strings with repeated "
+    "characters")
+{
+    string str1("asdfghjklñd");
+    string str2("ñlkjhgfdsad");
+    REQUIRE(isPermutation(str1, str2));
+    REQUIRE(isPermutationSort(str1, str2));
+}
+
+TEST_CASE("1.2 - Returns false with strings that are not permutated")
+{
+    string str1("asdfghjklñ");
+    string str2("ñlkjhgfdso");
     REQUIRE_FALSE(isPermutation(str1, str2));
     REQUIRE_FALSE(isPermutationSort(str1, str2));
 }
