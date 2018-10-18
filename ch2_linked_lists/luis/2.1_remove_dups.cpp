7f1a1c0b4c5ea664d123a2d779f60ac33811ff0d
Luis Díaz Más <piponazo@gmail.com>
2018-10-18 12:21:50 +0200

    Fix bugs in 2.1

--- ch2_linked_lists/luis/2.1_remove_dups.cpp
+++ ch2_linked_lists/luis/2.1_remove_dups.cpp
@@ -2,9 +2,9 @@
 // FOLLOW UP
 // How would you solve this problem if a temporary buffer is not allowed?
 
+#include "catch.hpp"
 #include "lists/Node.h"
 #include "lists/utils.h"
-#include "catch.hpp"
 
 #include <iostream>
 #include <unordered_set>
@@ -14,78 +14,81 @@ using namespace std;
 // Use a hash table to identify if a value has been already added:
 // Time complexity: O(N)
 // Space complexity: O(N)
-// This implementation uses extra space (unordered_set)
-// This implementation is not recursive.
-void removeDuplicates(Node* node) {
-    if (node == nullptr) {
-        return;
-    }
-
+void removeDuplicates(Node* node)
+{
     unordered_set<int> set;
-    set.insert(node->value);
+    Node* previous = nullptr;
 
-    while(node && node->next) {
-        auto& next = node->next;
-        bool duplicatedValue = !set.insert(next->value).second;
-
-        if (duplicatedValue) {
-            // This line do two things: resets next, and assign it to the next->next
-            next = move(next->next);
-        }
-
-        node = node->next.get();
-    }
-}
-
-// Instead of using a hash table, we have second pointer to search ahead the first one.
-// Time complexity: O(N^2)
-// This implementation does not use extra space.
-// This implementation is recursive.
-void removeDuplicatesNoBuffer(Node* node) {
-    if (node == nullptr) {
-        return;
-    }
-
-    Node* prev = node;
-    Node* runner = node->next.get();
-    while(runner) {
-        if (node->value == runner->value) {
-            prev->next = move(runner->next);
+    while (node != nullptr) {
+        auto ret = set.insert(node->value);
+        if (ret.second == false) {
+            previous->next = std::move(node->next);
+            node = previous->next.get();
         } else {
-            prev = runner;
+            previous = node;
+            node = node->next.get();
         }
-        runner = runner->next.get();
     }
-
-    removeDuplicatesNoBuffer(node->next.get());
 }
 
-TEST_CASE( "Remove duplicates when they are in non consecutive positions", "[2.1]" ) {
-    auto list = createList({5,1,2,3,4,5});
+// Temporary buffer not allowed -> ues a second pointer to search ahead the first one.
+// Time complexity: O(N^2)
+// Space complexity: O(1)
+void removeDuplicatesNoBuffer(Node* node)
+{
+    Node* current = node;
+    while (current)
+    {
+        Node* runner = current;
+        while (runner->next) {
+            if (runner->next->value == current->value) {
+                runner->next = std::move(runner->next->next);
+            } else {
+                runner = runner->next.get();
+            }
+        }
+        current = current->next.get();
+    }
+}
+
+TEST_CASE("Remove duplicates when they are in non consecutive positions", "[2.1]")
+{
+    auto list = createList({5, 1, 2, 3, 4, 5});
     REQUIRE(countNodes(list.get()) == 6);
 
-    SECTION("with hash table version") {
+    SECTION("with hash table version")
+    {
         removeDuplicates(list.get());
         REQUIRE(countNodes(list.get()) == 5);
     }
 
-    SECTION("with no space version") {
+    SECTION("with no space version")
+    {
         removeDuplicatesNoBuffer(list.get());
         REQUIRE(countNodes(list.get()) == 5);
     }
 }
 
-TEST_CASE( "Remove duplicates when they are in consecutive positions", "[2.1]" ) {
-    auto list = createList({1,2,2,2,3,4,5});
+TEST_CASE("Remove duplicates when they are in consecutive positions", "[2.1]")
+{
+    auto list = createList({1, 2, 2, 2, 3, 4, 5});
     REQUIRE(countNodes(list.get()) == 7);
 
-    SECTION("with hash table version") {
+    SECTION("with hash table version")
+    {
         removeDuplicates(list.get());
         REQUIRE(countNodes(list.get()) == 5);
     }
 
-    SECTION("with no space version") {
+    SECTION("with no space version")
+    {
         removeDuplicatesNoBuffer(list.get());
         REQUIRE(countNodes(list.get()) == 5);
     }
+}
+
+TEST_CASE("Do not crash with empty lists", "[2.1]")
+{
+    REQUIRE_NOTHROW(removeDuplicates(nullptr));
+    REQUIRE_NOTHROW(removeDuplicatesNoBuffer(nullptr));
 }
